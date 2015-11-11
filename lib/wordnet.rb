7466b432b8bd09c2935c507714463343e1afd5f0
Tristan Gamilis <tristan@2suggestions.com.au>
2015-11-11 19:03:49 +1100

    Remove pry and load path from wordnet

--- lib/wordnet.rb
+++ lib/wordnet.rb
@@ -1,8 +1,6 @@
 require 'pry'
-$:.unshift File.dirname(__FILE__)
 require 'wordnet/pointer'
 require 'wordnet/db'
 require 'wordnet/lemma'
 require 'wordnet/pointers'
 require 'wordnet/synset'
-binding.pry
